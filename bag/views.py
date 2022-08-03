38a51f6a79231f5ef3267d3e649ae39b2070019f
tdawes93 <tdawes136@googlemail.com>
2022-08-03 19:40:07 +0000

    Add toasts

--- bag/views.py
+++ bag/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 
+from products.models import Product
+
+# Create your views here.
 
 def view_bag(request):
-    """
-    A view to return the bag contents page
-    """
-    return render(request, "bag/bag.html")
+    """ A view that renders the bag contents page """
 
+    return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """Add quantity of specified product to bag"""
+    """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
-    if 'product.size' in request.POST:
+    if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
@@ -31,17 +34,18 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-
+    
 
 def adjust_bag(request, item_id):
-    """Adjust the quantity of specified product to the specified amount"""
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
-    if 'product.size' in request.POST:
+    if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
@@ -52,7 +56,6 @@ def adjust_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-
     else:
         if quantity > 0:
             bag[item_id] = quantity
@@ -64,11 +67,11 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """Remove the item from the bag"""
+    """Remove the item from the shopping bag"""
 
     try:
         size = None
-        if 'product.size' in request.POST:
+        if 'product_size' in request.POST:
             size = request.POST['product_size']
         bag = request.session.get('bag', {})
 
